7967383807c5e4452068f7b4cd81f9e8842ece50
Ivan Rodriguez <culma6794@gmail.com>
2022-10-20 14:57:04 -0500

    incorporando encriptacion

--- Controllers/EncriptarController.cs
+++ Controllers/EncriptarController.cs
@@ -6,11 +6,53 @@ namespace Prueba.Controllers
     [Route("api/[Controller]")]
     public class EncriptarController : Controller
     {
-        //[HttpGet("/encriptar/{num}")]
-        //public dynamic encriptar(int num)
-        //{
-        //    String[] abecedario = new String[];
-        //    return 
-        //}
+        char[] abecedario = "abcdefghijklmnñopqrstuvwxyzabcdefghijklmnñopqrstuvwxyz".ToCharArray();
+
+        [HttpPost("/encriptar")]
+        public dynamic encriptar(string p)
+        {
+            int num = 5;
+            char[] palabra = p.ToCharArray();
+            List<string> palabraEncriptada = new List<string>();
+            string letra = "";
+
+            for(int i = 0; i < palabra.Length; i++)
+            {
+                int indexLetra = Array.IndexOf(abecedario, palabra[i]);
+                letra = abecedario[indexLetra + num].ToString();
+                palabraEncriptada.Add(letra);
+            }
+            return new
+            {
+                palabra = string.Join( "", palabra),
+                encriptada = string.Join("", palabraEncriptada)
+
+            };
+        }
+
+        [HttpPost("/desencriptar")]
+        public dynamic desencriptar(string p)
+        {
+
+            int num = 5;
+            char[] palabra = p.ToCharArray();
+            List<string> palabraDecencriptada = new List<string>();
+            Array.Reverse(abecedario, 0, abecedario.Length);
+            string letra = "";
+
+
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                int indexLetra = Array.IndexOf(abecedario, palabra[i]);
+                letra = abecedario[indexLetra + num].ToString();
+                palabraDecencriptada.Add(letra);
+            }
+            return new
+            {
+                palabra = string.Join("", palabra),
+                decencriptada = string.Join("", palabraDecencriptada),
+                abecedario = abecedario
+            };
+        }
     }
 }
